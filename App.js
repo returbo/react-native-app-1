de8216f3977ad911b03845678c82bc51c8169ae6
returbo <returbo@mail.ru>
2020-08-07 16:43:22 +1000

    начало верстки

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import styled from "styled-components/native";
 
 export default function App() {
   return (
     <Container>
       <Group>
-        <GroupTitle></GroupTitle>
+        <GroupTitle>11 сентября</GroupTitle>
+        <GroupItem>
+          <Avatar
+            source={{
+              uri:
+                'https://sun1-99.userapi.com/impf/c855428/v855428811/b6432/26zhQD7ZgQU.jpg?size=50x0&quality=90&crop=0,0,470,470&sign=ef563fb12ad767b22590767eddcb1e47&ava=1'
+            }}
+          />
+        </GroupItem>
       </Group>
     </Container>
   );
 }
 
+const GroupItem = styled.View`
+  padding: 20px;
+`;
 
-const GroupTitle = styled.View`
+const Avatar = styled.Image`
+  border-radius: 50px;
+  width: 40px;
+  height: 40px;
+`;
 
+
+const GroupTitle = styled.Text`
+  font-weight: 800;
+  font-size: 22px;
+  color: #000000;
 `;
 
 const Group = styled.View`
