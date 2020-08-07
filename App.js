86763c0e4e6af0d70c087e66f61d952062ebd9d4
returbo <returbo@mail.ru>
2020-08-07 16:20:06 +1000

    инициализация + начало работы

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from "styled-components/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Group>
+        <GroupTitle></GroupTitle>
+      </Group>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const GroupTitle = styled.View`
+
+`;
+
+const Group = styled.View`
+
+`;
+
+const Container = styled.View`
+  flex: 1;
+  margin-top: 50px;
+`;
