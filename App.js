f7f7ec4147f5a5acfeb8731e7725e463ccbaab07
returbo <returbo@mail.ru>
2020-08-11 10:49:27 +1000

    верстка первой страницы с кнопкой добавить

--- App.js
+++ App.js
@@ -1,47 +1,134 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, SectionList } from 'react-native';
 import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
+
+import { Appointment, SectionTitle } from "./Components";
+
+const DATA = [
+  {
+    title: "11 августа",
+    data: [
+      {
+        time: '15.30',
+        diagnosis: 'я собственной персоны',
+        user: {
+          fullname: "Эрик Гукалов",
+          avatar: "https://sun9-30.userapi.com/impf/c855120/v855120608/b644b/eqW7OdZ0ArU.jpg?size=50x0&quality=90&crop=251,310,1064,1064&sign=f9061a3b13a3beee5518841ef2dae81b&ava=1"
+        },
+        active: true,
+      },
+      {
+        time: '15.30',
+        diagnosis: 'братан и дружбан',
+        user: {
+          fullname: "Эрик Гукалов",
+          avatar: "https://sun9-58.userapi.com/impf/c636827/v636827077/4e0bb/IHdIp6Hxcws.jpg?size=100x0&quality=90&crop=324,240,786,786&sign=03b064811652032c68b5f72de88989df&ava=1"
+        },
+      }
+    ]
+  },
+  {
+    title: "13 августа",
+    data: [
+      {
+        time: '15.30',
+        diagnosis: 'я собственной персоны',
+        user: {
+          fullname: "Эрик Гукалов",
+          avatar: "https://sun9-30.userapi.com/impf/c855120/v855120608/b644b/eqW7OdZ0ArU.jpg?size=50x0&quality=90&crop=251,310,1064,1064&sign=f9061a3b13a3beee5518841ef2dae81b&ava=1"
+        },
+      },
+      {
+        time: '15.30',
+        diagnosis: 'братан и дружбан',
+        user: {
+          fullname: "Эрик Гукалов",
+          avatar: "https://sun9-58.userapi.com/impf/c636827/v636827077/4e0bb/IHdIp6Hxcws.jpg?size=100x0&quality=90&crop=324,240,786,786&sign=03b064811652032c68b5f72de88989df&ava=1"
+        }
+      }
+    ],
+  },
+  {
+    title: "13 августа",
+    data: [
+      {
+        time: '15.30',
+        diagnosis: 'я собственной персоны',
+        user: {
+          fullname: "Эрик Гукалов",
+          avatar: "https://sun9-30.userapi.com/impf/c855120/v855120608/b644b/eqW7OdZ0ArU.jpg?size=50x0&quality=90&crop=251,310,1064,1064&sign=f9061a3b13a3beee5518841ef2dae81b&ava=1"
+        },
+      },
+      {
+        time: '15.30',
+        diagnosis: 'братан и дружбан',
+        user: {
+          fullname: "Эрик Гукалов",
+          avatar: "https://sun9-58.userapi.com/impf/c636827/v636827077/4e0bb/IHdIp6Hxcws.jpg?size=100x0&quality=90&crop=324,240,786,786&sign=03b064811652032c68b5f72de88989df&ava=1"
+        }
+      }
+    ],
+  },
+  {
+    title: "13 августа",
+    data: [
+      {
+        time: '15.30',
+        diagnosis: 'я собственной персоны',
+        user: {
+          fullname: "Эрик Гукалов",
+          avatar: "https://sun9-30.userapi.com/impf/c855120/v855120608/b644b/eqW7OdZ0ArU.jpg?size=50x0&quality=90&crop=251,310,1064,1064&sign=f9061a3b13a3beee5518841ef2dae81b&ava=1"
+        },
+      },
+      {
+        time: '15.30',
+        diagnosis: 'братан и дружбан',
+        user: {
+          fullname: "Эрик Гукалов",
+          avatar: "https://sun9-58.userapi.com/impf/c636827/v636827077/4e0bb/IHdIp6Hxcws.jpg?size=100x0&quality=90&crop=324,240,786,786&sign=03b064811652032c68b5f72de88989df&ava=1"
+        }
+      }
+    ],
+  },
+];
+
 
 export default function App() {
   return (
     <Container>
-      <Group>
-        <GroupTitle>11 сентября</GroupTitle>
-        <GroupItem>
-          <Avatar
-            source={{
-              uri:
-                'https://sun1-99.userapi.com/impf/c855428/v855428811/b6432/26zhQD7ZgQU.jpg?size=50x0&quality=90&crop=0,0,470,470&sign=ef563fb12ad767b22590767eddcb1e47&ava=1'
-            }}
-          />
-        </GroupItem>
-      </Group>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(_, index) => index}
+        renderItem={({ item }) => <Appointment{...item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <SectionTitle>{title}</SectionTitle>
+        )}
+      />
+      <PlusButton>
+        <Ionicons name="ios-add" size={36} color="white" />
+      </PlusButton>
     </Container>
   );
 }
 
-const GroupItem = styled.View`
-  padding: 20px;
-`;
-
-const Avatar = styled.Image`
+const PlusButton = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
   border-radius: 50px;
-  width: 40px;
-  height: 40px;
-`;
-
-
-const GroupTitle = styled.Text`
-  font-weight: 800;
-  font-size: 22px;
-  color: #000000;
-`;
-
-const Group = styled.View`
-
+  width: 64px;
+  height: 64px;
+  background: #2a86ff;
+  position: absolute;
+  right: 25px;
+  bottom: 25px;
+  shadow-color: black;
+  shadow-opacity: 0.7;
+  shadow-radius: 2.5;
+  elevation: 5;
 `;
 
 const Container = styled.View`
   flex: 1;
-  margin-top: 50px;
+  margin-top: 30px;
 `;
